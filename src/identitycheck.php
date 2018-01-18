6feb5f998a8411f0ce19d65958737be7a1af325c
lSpeak <tolga@tolgakarabulut.com.tr>
2018-01-19 01:11:00 +0300

    Fixed variables function

--- src/identitycheck.php
+++ src/identitycheck.php
@@ -50,7 +50,7 @@ class identitycheck {
 						<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
 							<soap:Body>
 								<TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
-									<TCKimlikNo>'.$identity.'</TCKimlikNo>
+									<TCKimlikNo>'.$number.'</TCKimlikNo>
 									<Ad>'.$name.'</Ad>
 									<Soyad>'.$surname.'</Soyad>
 									<DogumYili>'.$birtday.'</DogumYili>
